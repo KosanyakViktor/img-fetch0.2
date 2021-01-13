ec0c4c12da726ca1656dfbd2fab6b30e3b6255d2
KosanyakViktor <74109187+KosanyakViktor@users.noreply.github.com>
2021-01-13 16:03:51 +0200

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -1,11 +1,7 @@
 const spinnerElem = document.querySelector('.spinner');
-
-const showSpinner = () =>{
-spinnerElem.classList.remove('spinner_hidden');
-}
-
-const hideSpinner = () =>{
+export const hideSpinner = () => {
   spinnerElem.classList.add('spinner_hidden');
-  }
-
-export { showSpinner, hideSpinner };
+};
+export const showSpinner = () => {
+  spinnerElem.classList.remove('spinner_hidden');
+};
