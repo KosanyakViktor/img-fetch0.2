e6c0caca6b11409318548d64bc31fd343131cb9f
KosanyakViktor <74109187+KosanyakViktor@users.noreply.github.com>
2021-01-13 20:01:14 +0200

    Update gateways.js

--- gateways.js
+++ gateways.js
@@ -1,14 +1,17 @@
-export const fetchUserData = userName =>
-  fetch(`https://api.github.com/users/${userName}`).then(response => {
+export const fetchUserData = (userName) =>{
+  const response = await fetch(`https://api.github.com/users/${userName}`)
     if (response.ok) {
-      return response.json();
+      return await response.json();
     }
-    throw new Error('Failed to load data');
-  });
-export const fetchRepositories = url =>
-  fetch(url).then(response => {
+    throw new Error("Failed to load data");
+  };
+
+export const fetchRepositories = (url) => {
+  const response = await fetch(url);
     if (response.ok) {
-      return response.json();
+      return await response.json();
     }
-    throw new Error('Failed to load data');
-  });
+    throw alert(new Error("Failed to load data"));
+ };
+
+export { fetchUserData, fetchRepositories }
