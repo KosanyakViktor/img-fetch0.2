cb7e43167b96ea39b99e341dda43138ffd204649
KosanyakViktor <74109187+KosanyakViktor@users.noreply.github.com>
2021-01-13 16:01:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-import { fetchUserData, fetchRepositories } from './gateways.js'
-import { renderRepos } from './repos.js';
-import { renderUserData } from './user.js'
-import { showSpinner, hideSpinner } from './spinner.js'
-
-
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { showSpinner, hideSpinner } from './spinner.js';
 const defaultUser = {
-    avatar_url: 'https://avatars3.githubusercontent.com/ul10001',
-    name: '',
-    location: '',
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
 };
-
 renderUserData(defaultUser);
-
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
-
 const onSearchUser = () => {
-    showSpinner();
-    const userName = userNameInputElem.value;
-    fetchUserData(userName)
-         .then(userData => {
-             renderUserData(userData);
-             return userData.repos_url;
-         })
-         .then(url => fetchRepositories(url))
-         .then(reposList => { 
-             renderRepos(reposList);
-             hideSpinner();
-         })
-         .catch(() => {
-            hideSpinner();
-             alert(err.message);
-         })
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposList => {
+      renderRepos(reposList);
+    })
+    .catch(err => {
+      alert(err.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    });
 };
-
-showUserBtnElem.addEventListener('click', onSearchUser)
+showUserBtnElem.addEventListener('click', onSearchUser);
