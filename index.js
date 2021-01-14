3a83b265d1a62c3181988be9662d66b34bba0ac3
KosanyakViktor <74109187+KosanyakViktor@users.noreply.github.com>
2021-01-14 15:45:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-import { fetchUserData, fetchRepositories } from './gateways.js'
-import { renderRepos } from './repos.js';
-import { renderUserData } from './user.js'
-import { showSpinner, hideSpinner } from './spinner.js'
-
-
+import { fetchRepositories, fetchUserData } from "./gateways.js";
+import { cleanReposList, renderRepos } from "./repos.js";
+import { hideSpinner, showSpinner } from "./spinner.js";
+import { renderUserData } from "./user.js";
 const defaultUser = {
-    avatar_url: 'https://avatars3.githubusercontent.com/ul10001',
-    name: '',
-    location: '',
-};
-
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
+}
 renderUserData(defaultUser);
-
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
-
 const onSearchUser = async () => {
-    showSpinner();
-    const userName = userNameInputElem.value;
-    try {
-        const userData = await fetchUserData(userName);
-        renderUserData(userData);
-        const reposList = await fetchRepositories(userData.repos_url);
-        renderRepos(reposList);
-    } catch(err) {
-        alert(err.message);
-    } finally {
-        hideSpinner();
-    }
-
-
-    // fetchUserData(userName)
-    //      .then(userData => {
-    //          renderUserData(userData);
-    //          return userData.repos_url;
-    //      })
-    //      .then(url => fetchRepositories(url))
-    //      .then(reposList => { 
-    //          renderRepos(reposList);
-    //          hideSpinner();
-    //      })
-    //      .catch(() => {
-    //         hideSpinner();
-    //          alert(err.message);
-    //      })
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  try {
+    const userData = await fetchUserData(userName);
+    renderUserData(userData);
+    const reposList = await fetchRepositories(userData.repos_url);
+    renderRepos(reposList);
+  } catch (err) {
+    alert(err.message);
+  } finally {
+    hideSpinner();
+  }
+  /*   fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposList => {
+      renderRepos(reposList);
+      hideSpinner();
+    })
+    .catch(err => {
+      alert(err.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    })
+  */
 };
-
-showUserBtnElem.addEventListener('click', onSearchUser)
+showUserBtnElem.addEventListener('click', onSearchUser);
