24f77914aab857721798ac2ec77ae2e47069df8f
KosanyakViktor <74109187+KosanyakViktor@users.noreply.github.com>
2021-01-14 15:48:04 +0200

    Update repos.js

--- repos.js
+++ repos.js
@@ -1,14 +1,15 @@
 const listElem = document.querySelector('.repo-list');
 export const cleanReposList = () => {
   listElem.innerHTML = '';
-};
+}
 export const renderRepos = reposList => {
-  const reposListElems = reposList.map(({ name }) => {
-    const listItemElem = document.createElement('li');
-    listItemElem.classList.add('repo-list__item');
-    listItemElem.textContent = name;
-    return listItemElem;
-  });
+  const reposListElems = reposList
+    .map(({ name }) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('repo-list__item');
+      listItem.textContent = name;
+      return listItem;
+    });
   cleanReposList();
   listElem.append(...reposListElems);
 };
